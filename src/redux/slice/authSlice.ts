4495c9a893e167941e5dd82366f57a9209433ea2
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-24 12:45:52 -0300

    criando as fatias de slice do redux

---
+++ src/redux/slice/authSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { User } from "../../@types/Auth";
+
+interface AuthState {
+  user: User | null;
+  authToken: string | null;
+  AuthStatus: "authenticated" | "not_authenticated" | "not_verified";
+}
+
+const initialState: AuthState = {
+  user: null,
+  authToken: null,
+  AuthStatus: "not_authenticated",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<AuthState["user"]>) => {
+      state.user = action.payload;
+    },
+    setAuthToken: (state, action: PayloadAction<AuthState["authToken"]>) => {
+      state.authToken = action.payload;
+    },
+    setAuthState: (state, action: PayloadAction<AuthState["AuthStatus"]>) => {
+      state.AuthStatus = action.payload;
+    },
+  },
+});
+
+export const { setUser, setAuthToken, setAuthState } = authSlice.actions;
+
+export default authSlice.reducer;
