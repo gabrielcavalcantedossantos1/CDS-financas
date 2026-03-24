66ac85541a97017e76c22a108dd7966286ffd145
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-24 16:18:57 -0300

    aplicando o tema salvo pelo usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import { useAuth } from "./hooks/auth";
+import { useTheme } from "./hooks/theme";
 
 export default function App() {
   const { handleAuthenticateUser } = useAuth();
+  const { handleInitTheme } = useTheme();
 
   useEffect(() => {
     // chamando a funcao para autenticar o user ao carregar a pagina e salvando no localStorage
     handleAuthenticateUser();
+
+    // aplicar o tema salvo pelo usuario
+    handleInitTheme();
   }, []);
 
   return <div>...</div>;
