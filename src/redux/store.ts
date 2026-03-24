c1043ca2701384fb9baf6312690f07c2b51ae7b6
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-24 12:48:34 -0300

    adicionando o slice do auth na store do redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+  import { configureStore } from "@reduxjs/toolkit";
+  import  authSlice  from "./slice/authSlice";
 
-export const store = configureStore({
-  reducer: {},
-});
+  export const store = configureStore({
+    reducer: {
+      auth: authSlice,
+    },
+  });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+  export type RootState = ReturnType<typeof store.getState>;
+  export type AppDispatch = typeof store.dispatch;
