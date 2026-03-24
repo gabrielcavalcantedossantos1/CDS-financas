d4869c082968beab56dc80fb3f444216d99c240e
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-24 13:14:39 -0300

    funções para a autenticaçao do usuario

---
+++ src/hooks/auth.ts
@@ -0,0 +1,87 @@
+import type { User } from "../@types/Auth";
+import { useAppDispatch } from "../redux/hooks";
+import { setAuthStatus, setAuthToken, setUser } from "../redux/slice/authSlice";
+import { getUser, signIn, signUp } from "../services/requests";
+
+const LOCAL_STORAGE_AUTH_KEY = import.meta.env.VITE_LOCAL_STORAGE_AUTH_KEY;
+
+export const useAuth = () => {
+  const dispatch = useAppDispatch();
+
+  // funçao para autenticar o user
+  const authenticate = (user: User, authToken: string) => {
+    dispatch(setUser(user));
+    dispatch(setAuthToken(authToken));
+    dispatch(setAuthStatus("authenticated"));
+
+    localStorage.setItem(LOCAL_STORAGE_AUTH_KEY, authToken);
+  };
+
+  // pegando o token para o localStorage
+  const handleGetToken = () => localStorage.getItem(LOCAL_STORAGE_AUTH_KEY);
+
+  // pegando o usuario que esta la no localStorage
+  const handleAuthenticateUser = async () => {
+    const request = await getUser();
+    const authToken = handleGetToken();
+
+    if (!request.data) {
+      dispatch(setAuthStatus("not_authenticated"));
+      return;
+    }
+
+    const { data } = request;
+    authenticate(data.user, authToken as string);
+  };
+
+  // funçao para logar
+  const handleSignIn = async (email: string, password: string) => {
+    const request = await signIn(email, password);
+
+    if (request.data) {
+      const { data } = request;
+
+      authenticate(data.user, data.authToken);
+      return true;
+    }
+
+    dispatch(setAuthStatus("not_authenticated"));
+    return request.error;
+  };
+
+  // funçao para deslogar
+  const handleSignUp = async (
+    name: string,
+    email: string,
+    password: string,
+  ) => {
+    const request = await signUp(name, email, password);
+
+    if (request.data) {
+      const { data } = request;
+
+      authenticate(data.user, data.authToken);
+      return true;
+    }
+
+    dispatch(setAuthStatus("not_authenticated"));
+    return request.error;
+  };
+
+  // funçao para deslogar
+  const handleSignOut = () => {
+    dispatch(setUser(null));
+    dispatch(setAuthToken(null));
+    dispatch(setAuthStatus("not_authenticated"));
+
+    localStorage.removeItem(LOCAL_STORAGE_AUTH_KEY);
+  };
+
+  return {
+    handleGetToken,
+    handleAuthenticateUser,
+    handleSignIn,
+    handleSignUp,
+    handleSignOut,
+  };
+};
