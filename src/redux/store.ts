fb7ca47283394726d7c14bcdceecc401f999425d
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-24 16:06:25 -0300

    salvando o tema no store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
-  import { configureStore } from "@reduxjs/toolkit";
-  import  authSlice  from "./slice/authSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./slice/authSlice";
+import themeSlice from "./slice/themeSlice";
 
-  export const store = configureStore({
-    reducer: {
-      auth: authSlice,
-    },
-  });
+export const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    theme: themeSlice,
+  },
+});
 
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
