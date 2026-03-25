5dfd3a85cbb58c1137e8d58f63bfcebcf6edfea2
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-25 18:23:01 -0300

    Criando toda a estrutura do componente Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import { BiBorderAll } from "react-icons/bi";
+import { ButtonElement } from "./styles";
+
+type Props = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  size?: "sm" | "md";
+  borderRadius?: "rounded" | "sm" | "md";
+  width?: string;
+};
+
+export function Button({
+  children,
+  onClick,
+  size = "sm",
+  borderRadius = "sm",
+  width = "100%",
+}: Props) {
+  return (
+    <ButtonElement
+      onClick={onClick}
+      $size={size}
+      $borderRadius={borderRadius}
+      $width={width}
+    >
+      {children}
+    </ButtonElement>
+  );
+}
