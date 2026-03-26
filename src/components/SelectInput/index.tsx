1daa5fba77d0a51e5d4b3944c35b16df3483c4ad
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-26 07:26:22 -0300

    Criando o componente SelectInput

---
+++ src/components/SelectInput/index.tsx
@@ -0,0 +1,32 @@
+import { useId, type ChangeEvent } from "react";
+import { Container, Label, Select } from "./styles";
+
+type Options = {
+  value: string;
+  label: string;
+};
+
+type Props = {
+  value: string;
+  options: Options[];
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  label?: string;
+};
+
+const referenceId = useId();
+
+export function SelectInput({ onChange, options, value, label }: Props) {
+  return (
+    <Container>
+      {label && <Label htmlFor={referenceId}>{label}</Label>}
+
+      <Select id={referenceId} onChange={onChange} value={value}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </Select>
+    </Container>
+  );
+}
