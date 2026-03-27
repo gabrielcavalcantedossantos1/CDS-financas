9e575fcd3c62c00f6f7bfc27760c32ca2f62cf8f
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-27 13:12:24 -0300

    apenas testando o meu site

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Auth } from "../pages/Auth";
+import { Layout } from "../components/Layout/indez";
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/signin" element={<Auth type="signIn" />} />
       <Route path="/signup" element={<Auth type="signUp" />} />
+      <Route element={<Layout />}>
+        <Route index element={<div>Esse é a página inicial</div>} />
+      </Route>
     </Routes>
   );
 };
