3d123da0db17057970e4f9f8b1bf942632d20baf
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-28 15:01:09 -0300

    estilo de Edit

---
+++ src/pages/Transaction/Edit/styles.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 40px 50px;
+  border-bottom: 1px solid ${(props) => props.theme.COLORS.borderColor};
+`;
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  min-width: 0;
+`;
+
+export const HeaderTitle = styled.span`
+  font-size: ${(props) => props.theme.FONT_SIZES.xl};
+  color: ${(props) => props.theme.COLORS.textColor500};
+  font-weight: 800;
+`;
+
+export const HeaderSubTitle = styled.span`
+  font-size: ${(props) => props.theme.FONT_SIZES.sm};
+  color: ${(props) => props.theme.COLORS.textColor400};
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px 50px;
+  gap: 20px;
+`;
+
+export const Footer = styled.div`
+  padding: 5px 50px;
+`;
