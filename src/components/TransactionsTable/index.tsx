564a53b232c7ddfc2fee8c702dce818e978a3c01
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-29 21:52:34 -0300

    finalizando o componente transactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,83 @@
+import { useTheme } from "styled-components";
+import type { Transaction } from "../../@types/Transaction";
+import {
+  ActionBtn,
+  Actions,
+  Container,
+  DeleteIcon,
+  EditIcon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeadCell,
+  TableHeader,
+  TableRow,
+} from "./styles";
+import { formatDate } from "../../utils/formatDate";
+import { formatValue } from "../../utils/formatValue";
+
+type Props = {
+  data: Transaction[];
+  onEdit: (id: number) => void;
+  onDelete: (id: number) => void;
+};
+
+export function TransactionsTable({ data, onEdit, onDelete }: Props) {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeadCell style={{ width: 1 }}>#ID</TableHeadCell>
+            <TableHeadCell>Título</TableHeadCell>
+            <TableHeadCell>Data</TableHeadCell>
+            <TableHeadCell>Status</TableHeadCell>
+            <TableHeadCell>Valor</TableHeadCell>
+            <TableHeadCell style={{ width: 1 }}>Ações</TableHeadCell>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {data.map((transaction) => (
+            <TableRow key={transaction.id}>
+              <TableCell>${transaction.id}</TableCell>
+              <TableCell>{transaction.title}</TableCell>
+              <TableCell>{formatDate(transaction.created_at)}</TableCell>
+              <TableCell>
+                {transaction.status == "pending" ? "Pendente" : "Concluida"}
+              </TableCell>
+              <TableCell
+                style={{
+                  color:
+                    transaction.amount >= 0
+                      ? theme.COLORS.success
+                      : theme.COLORS.danger,
+                }}
+              >
+                {formatValue(transaction.amount)}
+              </TableCell>
+              <TableCell>
+                <Actions>
+                  <ActionBtn
+                    $variant="warning"
+                    onClick={() => onEdit(transaction.id)}
+                  >
+                    <EditIcon />
+                  </ActionBtn>
+                  <ActionBtn
+                    $variant="danger"
+                    onClick={() => onDelete(transaction.id)}
+                  >
+                    <DeleteIcon />
+                  </ActionBtn>
+                </Actions>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+}
