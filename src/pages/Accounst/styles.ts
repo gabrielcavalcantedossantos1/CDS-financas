0c8cb2696a87e83e5dac199260e1f080e60ac279
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-03-31 08:35:49 -0300

    estilo da pagina account

---
+++ src/pages/Accounst/styles.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 40px 50px;
+  border-bottom: 1px solid ${(props) => props.theme.COLORS.borderColor};
+`;
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  min-width: 0;
+`;
+
+export const HeaderTitle = styled.span`
+  font-size: ${(props) => props.theme.FONT_SIZES.xl};
+  color: ${(props) => props.theme.COLORS.textColor500};
+  font-weight: 800;
+`;
+
+export const HeaderSubTitle = styled.span`
+  font-size: ${(props) => props.theme.FONT_SIZES.sm};
+  color: ${(props) => props.theme.COLORS.textColor400};
+`;
+
+export const HeaderDeleteAccount = styled.div``;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 30px 50px;
+`;
+
+export const Footer = styled.div`
+  padding: 5px 50px;
+`;
